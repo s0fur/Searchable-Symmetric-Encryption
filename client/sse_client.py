23b0bb24121710c0df1151242babbe1c92fb1193
Ian Van Houdt <ifv@pdx.edu>
2015-07-09 21:37:52 -0700

    Added Encryption for client module
    Client module can now encrypt txt files supplied via cli, and
    outputs the encrypted text in another file supplied via cli.
    
    python sse_client.py -e [file input] [file output]

--- client/sse_client.py
+++ client/sse_client.py
@@ -16,6 +16,7 @@ from Crypto.Hash import HMAC
 from Crypto.Cipher import AES
 from Crypto import Random
 import bcrypt
+from argparse import ArgumentParser
 
 DEBUG = 1
 
@@ -82,11 +83,68 @@ class Client_SSE():
 
         return cipher
 
+    def encryptMail(self, infile, outfile):
+        # python sse_client.py -e ../mail/message1.txt encM1.txt
+
+        buf = infile.read()
+        if buf == '': 
+            print("[Enc] mail to encrypt is empty!\nExiting\n")
+            exit(1)
+
+        if (DEBUG): print("[Enc] mail to encrypt: %s\n" % (buf))
+
+        #method = sse.cipher.encrypt
+
+        while len(buf)%16 != 0:
+            buf = buf + "\x08"
+
+        #outfile.write(method(buf))
+        outfile.write(self.cipher.encrypt(buf))
+        outfile.close()        
+
+    def decryptMail(self, mailName):
+
+        pass
 
 def debugEcho(msg):
     if (DEBUG):
         print ("[Client-BackEnd] Msg from client: %s" % (msg))
 
 
+def main():
 
-sse = Client_SSE()
+    # Set-up a command-line argument parser
+    parser = ArgumentParser(description=__doc__, epilog="""Input is read from
+        stdin and output is written to stdout. Use the stream redirection
+        features of your shell to pass data through this program. If a key is
+        not specified, it is generated and written to stderr.""")
+    parser.add_argument('-s', '--search', action='store_true')
+    parser.add_argument('-u', '--update', action='store_true')
+    parser.add_argument('-e', '--encrypt', metavar='encrypt_file', 
+                        dest='encrypt_file', nargs='*')
+    parser.add_argument('-d', '--decrypt', action='store_true')
+    parser.add_argument('-k', '--key', metavar='key')
+    args = parser.parse_args()
+    print args
+ 
+    sse = Client_SSE()
+
+    # Decode the key if it was supplied
+    # key = base64.b64decode(args.key) if args.key else None
+
+
+    if args.encrypt_file:
+        infile = open(args.encrypt_file[0], "r")        
+        outfile = open(args.encrypt_file[1], "w+")
+        sse.encryptMail(infile, outfile)
+
+    elif args.decrypt:
+        pass
+
+    else:
+        print "Must specify encrypt or decrypt!"
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
