e2aa0c2aeff769a3216371893e41a9217ec8dc77
Ian Van Houdt <ifv@pdx.edu>
2015-07-10 19:52:35 -0700

    Added decryption of messages
    Files can now be encrypted and decrypted via command line. Only
    tested with small messages. Key is static, which isn't a good
    idea but it gets it working for now.

--- client/sse_client.py
+++ client/sse_client.py
@@ -17,6 +17,7 @@ from Crypto.Cipher import AES
 from Crypto import Random
 import bcrypt
 from argparse import ArgumentParser
+import binascii
 
 DEBUG = 1
 
@@ -35,6 +36,14 @@ class Client_SSE():
         # as an arg of some sort
         self.password = b"password"
         self.salt = 42
+
+        # TODO: need to sort out use of salt. Previously, salt was
+        # randomly generated in initKeys, but the resulting pass-
+        # words k & kPrime were different on each execution, and 
+        # decryption was impossible. Hardcoding salt makes dectyption
+        # possible but may be a bad short cut
+        self.iv = None
+        self.salt = "$2b$12$ddTuco8zWXF2.kTqtOZa9O"
 
         # Two keys, generated/Initialized by KDF
         (self.k, self.kPrime) = self.initKeys()
@@ -55,17 +64,18 @@ class Client_SSE():
         # k used for PRF; kPrime used for Enc/Dec
         # return (k, kPrime)
 
-        #password = b"password"
-
-        hashed = bcrypt.hashpw(self.password, bcrypt.gensalt())
+        #hashed = bcrypt.hashpw(self.password, bcrypt.gensalt())
+        hashed = bcrypt.hashpw(self.password, self.salt)
 
         if bcrypt.hashpw(self.password, hashed) == hashed:
-            if (DEBUG): print "PW match!"
+            pass
         else:
-            if (DEBUG): print "No PW match!"
+            print "Password hash failed, exiting"
             exit(1)
 
-        if(DEBUG): print("len of k = %d" % len(hashed))
+        if(DEBUG > 1):
+            print("len of k = %d" % len(hashed))
+            print("k = %s" % hashed)
 
         # Currently k and kPrime are ==
         # TODO: how to do the two keys?
@@ -78,33 +88,43 @@ class Client_SSE():
         # TODO: fix key. Currently just a hack: AES keys must be
         # 16, 24 or 32 bytes long, but kPrime is 60
         key = self.kPrime[:16]
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(key, AES.MODE_CBC, iv)
+
+        # generates 16 byte random iv
+        self.iv = Random.new().read(AES.block_size)
+
+        cipher = AES.new(key, AES.MODE_CBC, self.iv)
 
         return cipher
 
     def encryptMail(self, infile, outfile):
-        # python sse_client.py -e ../mail/message1.txt encM1.txt
+        # python sse_client.py -e ../mail/msg1.txt enc_msg1.txt
 
         buf = infile.read()
         if buf == '': 
             print("[Enc] mail to encrypt is empty!\nExiting\n")
             exit(1)
 
-        if (DEBUG): print("[Enc] mail to encrypt: %s\n" % (buf))
-
-        #method = sse.cipher.encrypt
+        if (DEBUG > 1): print("[Enc] mail to encrypt: %s\n" % (buf))
 
         while len(buf)%16 != 0:
             buf = buf + "\x08"
 
-        #outfile.write(method(buf))
-        outfile.write(self.cipher.encrypt(buf))
-        outfile.close()        
+        outfile.write(self.iv + self.cipher.encrypt(buf))
 
-    def decryptMail(self, mailName):
+    def decryptMail(self, infile, outfile):
+        # python sse_client -d enc_msg1.txt dec_msg1.txt
 
-        pass
+        buf = infile.read()
+        if buf == '': 
+            print("[Dec] mail to decrypt is empty!\nExiting\n")
+            exit(1)
+
+        # self.kPrime[:16] == first 16 bytes of kPrime, ie: enc key
+        # buf[:16] == iv of encrypted msg
+        cipher = AES.new(self.kPrime[:16], AES.MODE_CBC, buf[:16])
+
+        # decrypt all but first 16 bytes (iv)
+        outfile.write(cipher.decrypt(buf[16:]))
 
 def debugEcho(msg):
     if (DEBUG):
@@ -114,32 +134,46 @@ def debugEcho(msg):
 def main():
 
     # Set-up a command-line argument parser
-    parser = ArgumentParser(description=__doc__, epilog="""Input is read from
-        stdin and output is written to stdout. Use the stream redirection
-        features of your shell to pass data through this program. If a key is
-        not specified, it is generated and written to stderr.""")
+    parser = ArgumentParser()
     parser.add_argument('-s', '--search', action='store_true')
     parser.add_argument('-u', '--update', action='store_true')
     parser.add_argument('-e', '--encrypt', metavar='encrypt_file', 
                         dest='encrypt_file', nargs='*')
-    parser.add_argument('-d', '--decrypt', action='store_true')
+    parser.add_argument('-d', '--decrypt', metavar='decrypt_file',
+                        dest='decrypt_file', nargs='*')
     parser.add_argument('-k', '--key', metavar='key')
     args = parser.parse_args()
-    print args
  
     sse = Client_SSE()
 
     # Decode the key if it was supplied
     # key = base64.b64decode(args.key) if args.key else None
 
-
     if args.encrypt_file:
+        if (DEBUG): 
+            print("Encrypting %s\nOutput %s\n"
+            % (args.encrypt_file[0], args.encrypt_file[1]))
+
         infile = open(args.encrypt_file[0], "r")        
         outfile = open(args.encrypt_file[1], "w+")
+
         sse.encryptMail(infile, outfile)
 
-    elif args.decrypt:
-        pass
+        infile.close()
+        outfile.close()
+
+    elif args.decrypt_file:
+        if (DEBUG): 
+            print("Decrypting %s\nOutput %s" 
+            % (args.decrypt_file[0], args.decrypt_file[1]))
+
+        infile = open(args.decrypt_file[0], "r")
+        outfile = open(args.decrypt_file[1], "w+")
+
+        sse.decryptMail(infile, outfile)
+
+        infile.close()
+        outfile.close()
 
     else:
         print "Must specify encrypt or decrypt!"
