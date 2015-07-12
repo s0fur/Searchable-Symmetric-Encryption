e929380b28059d6d6f9f02050f1d6c86026d0e0b
Ian Van Houdt <ifv@pdx.edu>
2015-07-12 10:27:53 -0700

    Added update method on client
    Created update routine for client backend that builds an encrypted
    index out of an input message. Writes that index to a local db
    maintained by the anydbm module.
    
    Not tested for decryption, though it compiles and creates consistent
    indexes via HMAC

--- client/sse_client.py
+++ client/sse_client.py
@@ -13,11 +13,15 @@ import socket
 import os
 import sys
 from Crypto.Hash import HMAC
+from Crypto.Hash import SHA256
 from Crypto.Cipher import AES
 from Crypto import Random
 import bcrypt
 from argparse import ArgumentParser
 import binascii
+import string
+import anydbm
+from client import Client
 
 DEBUG = 1
 
@@ -30,12 +34,13 @@ class Client_SSE():
 
     def __init__(self):
 
+        self.client = Client.new()
+
         # need to either set up index, or load it in from file
 
         # placeholder for password. Will eventually take
         # as an arg of some sort
         self.password = b"password"
-        self.salt = 42
 
         # TODO: need to sort out use of salt. Previously, salt was
         # randomly generated in initKeys, but the resulting pass-
@@ -47,10 +52,6 @@ class Client_SSE():
 
         # Two keys, generated/Initialized by KDF
         (self.k, self.kPrime) = self.initKeys()
-
-        # Pseudorandom func: HMAC.
-        # default digestmod is Crypto.Hash.MD5 
-        self.prf = HMAC.new(self.k)
 
         # Two K's: generated/initialized by PRF
         self.k1 = None
@@ -126,6 +127,99 @@ class Client_SSE():
         # decrypt all but first 16 bytes (iv)
         outfile.write(cipher.decrypt(buf[16:]))
 
+    def update(self, document):
+
+        infile = open(document, "r")
+        word_list = self.parseDocument(infile)
+        if (DEBUG > 1): print "[Update] Words from doc: " + word_list
+        infile.close()
+
+        index = self.encryptIndex(document, word_list)
+
+        # send updated index to server, as well as the encr messages
+
+    def parseDocument(self, infile):
+
+        word_list = None
+
+        for line in infile:
+            for word in line.split():
+                try:
+                    if word not in word_list and '\x08' not in word:
+                        word_list.append(word)
+                # except catches case of first word in doc, and an
+                # empty list cannot be iterated over
+                except:
+                    word_list = [word]
+
+        return word_list
+
+    def encryptIndex(self, document, word_list):
+
+        if (DEBUG): print "Encrypting index of words in %s" % document
+
+        L = []
+        # kPlus as described below (first) is used in the implementation
+        # of Dynamic SSE (specifically to allow updating, and requires
+        # mult dicts). For simplicity, I'm first using a basic version
+        # where I only maintain 1 dictionary on the server and on the 
+        # client.  
+        # Additionally, it seems the 2nd dict is just to manage updates
+        # that are added after the initial setup, but since I'm not 
+        # (yet) implementing setup(), two dictionaries is unecessary
+
+        index = anydbm.open("index", "c")
+
+        dynamic = 0 # 0 for 1 dict, 1 for 2 dicts (not implemented)
+
+        if dynamic > 0:
+            kPlus = self.prf.update(self.k + "3").digest()
+            for w in word_list:
+                print "kPlus = " + kPlus + " w = " + w
+                k1Plus = self.prf.update(kPlus + ("1" + w))
+                k2Plus = self.prf.update(kPlus + ("2" + w))
+        
+        else:
+            for w in word_list:
+
+                # Initialize K1 and K2
+                k1 = self.PRF(self.k, ("1" + w))
+                k2 = self.PRF(self.k, ("2" + w))
+
+                if (DEBUG): print("k1 = %s\nk2 = %s\n" % (k1, k2))
+
+                # Set counter "c" (set as 0 if not in index)
+                c = 0
+                found = 0
+                for k, v in index.iteritems():
+                    if k == w:
+                        if (DEBUG): 
+                            print("Found %s in db. C = %d" % (w, c))
+                            found = 1
+                            break
+                    else:
+                        c = c + 1
+
+                if (DEBUG and not found):
+                    print("'%s' not found in db" % w)
+
+                l = self.PRF(k1, str(c))
+                d = self.PRF(k2, document)
+
+                if (DEBUG):
+                    print("l = %s\nd = %s\n" % (l, d))
+                    print "w = " + w + "\tc = " + str(c)
+
+                c = c + 1
+                index[w] = str(c)
+                L.append((l, d))
+
+        return L
+
+    def PRF(self, k, data):
+        hmac = HMAC.new(k, data, SHA256)
+        return hmac.hexdigest()
+
 def debugEcho(msg):
     if (DEBUG):
         print ("[Client-BackEnd] Msg from client: %s" % (msg))
@@ -136,12 +230,14 @@ def main():
     # Set-up a command-line argument parser
     parser = ArgumentParser()
     parser.add_argument('-s', '--search', action='store_true')
-    parser.add_argument('-u', '--update', action='store_true')
+    parser.add_argument('-u', '--update', metavar='update', dest='update',
+                        nargs=1)
     parser.add_argument('-e', '--encrypt', metavar='encrypt_file', 
-                        dest='encrypt_file', nargs='*')
+                        dest='encrypt_file', nargs=2)
     parser.add_argument('-d', '--decrypt', metavar='decrypt_file',
-                        dest='decrypt_file', nargs='*')
+                        dest='decrypt_file', nargs=2)
     parser.add_argument('-k', '--key', metavar='key')
+    parser.add_argument('-i', '--inspect index', dest='inspect_index')
     args = parser.parse_args()
  
     sse = Client_SSE()
@@ -174,6 +270,20 @@ def main():
 
         infile.close()
         outfile.close()
+
+    elif args.update:
+        if (DEBUG):
+            print("Updating index with document %s" % args.update[0])
+
+        sse.update(args.update[0])
+
+    elif args.inspect_index:
+        if (DEBUG): print("Inspecting the index")
+        index = anydbm.open("index", "r")
+        for k, v in index.iteritems():
+            print "k:%s\tv:%s" % (k, v)
+
+        index.close()
 
     else:
         print "Must specify encrypt or decrypt!"
