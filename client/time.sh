3536ca28667759aaabb91c1ed2d69bb9232bf8d8
Ian Van Houdt <ifv@pdx.edu>
2015-08-03 21:17:29 -0700

    Added script to measure execution time

---
+++ client/time.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+UPDATE='python sse_client.py -u'
+SEARCH='python sse_client.py -s'
+
+# 10 files to UPDATE
+FILES=('1.' '12.' '15.' '18.' '20.' '23.' '26.' '29.' '31.' '34.') # '37.' '4.' '42.' '45.' '62.' '65.' '68.' '70.' '73.' '78.') # '83.' '86.');
+
+# 10 'known' terms
+TERMS=('tuesday') # 'such' 'pm' 'message' 'if' 'but' 'very' 'problem' 'see' 'the')
+
+# 5 'unknown' terms
+UNKNOWN=('beluga' 'whippersnapper' 'cufflink' 'grigsby' 'described')
+
+
+rm index*
+rm ../server/index
+for i in "${FILES[@]}"; do
+    echo "$UPDATE inbox/$i"
+    $UPDATE inbox/$i
+done
+exit 0
+
+for i in "${TERMS[@]}"; do
+    echo "$SEARCH $i"
+    $SEARCH $i
+done
+exit 0
+
+for i in "${UNKNOWN[@]}"; do
+    echo "$SEARCH $i";
+    $SEARCH $i
+done
